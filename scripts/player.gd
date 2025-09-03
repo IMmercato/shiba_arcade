8290ca42b7671bf9a0409656067f4c5c62f0031d
IMmercato <imesh.fernando100@gmail.com>
2025-09-03 18:21:20 +0200

    Audio added for Player per scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,38 @@ extends CharacterBody3D
 const SPEED := 2.5
 const JUMP_VELOCITY := 5
 
-func _physics_process(delta: float) -> void: # Add the gravity.
+func _ready():
+	# Add to player group
+	add_to_group("player")
+
+func _physics_process(delta: float) -> void:
 	if not is_on_floor(): 
-		$player/AnimationPlayer.play("Human Armature|Jump") 
 		velocity += get_gravity() * delta
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor(): 
 		velocity.y = JUMP_VELOCITY 
-	# Get the input direction and handle the movement/deceleration. 
-	# As good practice, you should replace UI actions with custom gameplay actions. 
+		$player/AnimationPlayer.play("Human Armature|Jump")
+		_play_mario_sound("jump")
+	
 	var input_dir := Input.get_vector("left", "right", "front", "back") 
 	var direction := (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized() 
+	
 	if direction: 
 		velocity.x = direction.x * SPEED 
 		velocity.z = direction.z * SPEED 
-		$player/AnimationPlayer.play("Human Armature|Walk") 
+		$player/AnimationPlayer.play("Human Armature|Walk")
 	else: 
 		velocity.x = move_toward(velocity.x, 0, SPEED) 
 		velocity.z = move_toward(velocity.z, 0, SPEED) 
+	
 	move_and_slide()
+
+func _play_mario_sound(sound_name: String):
+	# Check if we have Mario sounds and audio player
+	if has_meta("mario_sounds") and has_node("MarioAudio"):
+		var mario_sounds = get_meta("mario_sounds")
+		var audio_player = get_node("MarioAudio")
+		
+		if mario_sounds.has(sound_name) and audio_player:
+			audio_player.stream = mario_sounds[sound_name]
+			audio_player.play()
--- scripts/super_mario.gd
+++ scripts/super_mario.gd
@@ -1,3 +1,52 @@
 extends Node3D
 
-@export var walls_scene: PackedScene
+# Reference to the player
+var player: CharacterBody3D = null
+
+func _ready():
+	# Get the Area3D node and connect signals
+	var area = $Area3D
+	area.body_entered.connect(_on_body_entered)
+	area.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node):
+	if body.is_in_group("player"):
+		player = body
+		print("Player entered Mario level")
+		# Connect to player signals or add audio component
+		_setup_player_audio()
+
+func _on_body_exited(body: Node):
+	if body.is_in_group("player"):
+		print("Player left Mario level")
+		_cleanup_player_audio()
+		player = null
+
+func _setup_player_audio():
+	if player:
+		# Add an AudioStreamPlayer3D to the player if it doesn't have one
+		if not player.has_node("MarioAudio"):
+			var audio_player = AudioStreamPlayer3D.new()
+			audio_player.name = "MarioAudio"
+			player.add_child(audio_player)
+			print("MarioAudio added to player")
+		else:
+			print("MarioAudio already exists on player")
+		
+		# Load Mario sounds
+		var mario_sounds = {
+			"jump": preload("res://audio/Mario Jump - QuickSounds.com.mp3")
+		}
+		
+		# Store sounds in player for access
+		player.set_meta("mario_sounds", mario_sounds)
+		print("Mario sounds loaded")
+
+func _cleanup_player_audio():
+	if player and player.has_node("MarioAudio"):
+		var audio_player = player.get_node("MarioAudio")
+		if audio_player:
+			audio_player.queue_free()
+		if player.has_meta("mario_sounds"):
+			player.remove_meta("mario_sounds")
+		print("Mario audio cleaned up")
