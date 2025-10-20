234ff8fd328c0fc6f0c2627de23cba416580216f
IMmercato <imesh.fernando100@gmail.com>
2025-10-20 18:14:37 +0200

    Menu Continue button

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,3 +5,7 @@ func _on_start_pressed() -> void:
 
 func _on_leave_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_continue_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
